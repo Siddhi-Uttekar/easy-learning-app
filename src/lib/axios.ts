22160b9a6e73e009dbd5b08d7ecfa9efb33ebeaa
Siddhi-Uttekar <uttekarsiddhi145@gmail.com>
2025-06-20 14:42:06 +0530

    created axios, authSlice,  redux store file.

---
+++ src/lib/axios.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import {store} from '../store/index';
+import { logout } from '../store/slices/authSlice';
+
+const api = axios.create({
+    baseURL: "/api",
+    headers: {
+        'Content-Type' : 'application/json',
+    },
+});
+
+api.interceptors.request.use((config)=> {
+    const token = store.getState().auth.token;
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+api.interceptors.response.use(
+    response => response,
+    error => {
+        const { status } = error.response || {};
+        if (status === 401 || status === 403) {
+            store.dispatch(logout());
+        }
+        return Promise.reject(error);
+    }
+);
