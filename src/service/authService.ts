e7c9119e32683cf323c0b923cba3398e86a34f14
Siddhi-Uttekar <uttekarsiddhi145@gmail.com>
2025-06-23 22:17:31 +0530

    changed accessToken to access_token

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -10,22 +10,25 @@ class AuthService {
   }
 
   async verifyOtp(email: string, otp: string) {
-    try {
-      const res = await api.post('/auth/verify-otp', { email, otp });
-      const { accessToken, isNewUser } = res.data;
+  try {
+    const res = await api.post('/auth/verify-otp', { email, otp });
+    const { access_token, isNewUser } = res.data;
 
-      // Save token to Redux
-      store.dispatch(loginSuccess({ accessToken }));
+    console.log("token", access_token);
 
-      // Save token to LocalStorage
-      localStorage.setItem('accessToken', accessToken);
+    // Save token to Redux
+    store.dispatch(loginSuccess({ accessToken: access_token }));
 
-      return { isNewUser };
-    } catch (error) {
-      console.error('OTP verification failed:', error);
-      throw error;
-    }
+    // Save token to LocalStorage
+    localStorage.setItem('accessToken', access_token);
+
+    return { isNewUser };
+  } catch (error) {
+    console.error('OTP verification failed:', error);
+    throw error;
   }
+}
+
 
   isLoggedIn() {
     const accessToken = store.getState().auth.accessToken;
